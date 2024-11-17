05e950b8a823e5461cd581c009558bc7d54d5d47
Karl Schoch <karl.schoch@gmail.com>
2024-11-17 15:18:54 -0500

    Updates upload handler to save file to location on disk and return filepath

--- missourai_django/transcription/views.py
+++ missourai_django/transcription/views.py
@@ -7,30 +7,27 @@ import os
 # Create handlers here
 def handle_uploaded_file(f, name):
     # Define path where file will be saved and create directory if it doesn't exist
-    directory = 'audio_files'
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-
-    path = os.path.join(directory, name)
-    print(f"path writing to: {path}") # within root directory of the container
     # Need to do some validation to ensure
     ## File is an audio file
     ## name doesn't already exist in the directory
+    directory = 'audio_files'
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    path = os.path.join(directory, name)
+    print(f"path writing to: {path}") # within root directory of the container
 
     # Write file to path
     with open(path, 'wb+') as destination:
-        i = 1
-        for chunk in f.chunks():
-            print(f"writing chunk {i}")
-            destination.write(chunk)
-            i += 1
+        file_content = f.read()
+        destination.write(file_content)
 
-    # Perform additional logic here (i.e. transcription)
-    ## Target: Transcribe each chunk into a single text file and create a new text file you save to and return from
-    ## For now, ...
+    # Create transcript of audio file
+    transcript_path = f"{path}_transcript.txt"
+    with open(transcript_path, 'wb+') as destination:
+        destination.write(b"trancsript of audio file")
 
-    # Return path to enable creating db record
-    #return path
+    # Return path to transcript enable creating db record
+    return transcript_path
 
 # Create your views here.
 def index(request):
@@ -45,12 +42,13 @@ def upload_audio(request):
         if form.is_valid():
             # Save form instance but don't commit to db yet
             audio_file = form.save(commit=False)
-            # Print statements to check
-            print(request.FILES['file'])
-            print("run handle_uploaded_file function")
-            handle_uploaded_file(request.FILES['file'], audio_file.name)
+            # Create transcript of audio file, returning path to transcript
+            transcript_path = handle_uploaded_file(request.FILES['file'], audio_file.name)
+            print(f"transcript_path: {transcript_path}")
+            # Create transcript record in db
             #return redirect('success')
-            return redirect('transcription:upload_audio')
+            #return redirect('transcription:upload_audio')
+            return redirect('transcription:transcripts')
     else:
         form = AudioFileForm()
     return render(request, 'transcription/upload.html', {'form': form})
