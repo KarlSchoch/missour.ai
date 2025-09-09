a269fcf0be3d51a521da578adeeb68e22f7b6d35
Karl Schoch <karl.schoch@gmail.com>
2025-09-09 08:15:35 -0400

    Creates basic UI scaffolding for integrating a React application into Django template, but need to work through issues created with docker-compose

--- missourai_django/transcription/ui_urls.py
+++ missourai_django/transcription/ui_urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('upload/', views.upload_audio, name='upload_audio'),
     path('transcripts/', views.transcripts, name='transcripts'),
     path('transcripts/<int:transcript_id>/', views.view_transcript, name='view_transcript'),
+    path('dashboard/', views.dashboard, name='dashboard'),
 ]
