4646bc16ef2943f864f9806567d7243fd774a4a1
Karl Schoch <karl.schoch@gmail.com>
2025-11-04 04:33:33 -0500

    completely ports over topic selection functionality to new component

--- frontend/src/analyze-audio-page-section.jsx
+++ frontend/src/analyze-audio-page-section.jsx
@@ -11,78 +11,20 @@ function getInitialData() {
 function App() {
     const init = React.useMemo(getInitialData, []);
     const [conductAnalysis, setConductAnalysis] = useState(false);
-    // const [selected, setSelected] = useState(() => new Set([]))
-    // const hiddenRef = useRef(null);
-
-    // // Keep value of hiddenRef in line with the selected
-    // useEffect(() => {
-    //     if (hiddenRef.current) {
-    //         hiddenRef.current.value = JSON.stringify(Array.from(selected));
-    //     }
-    // })
-
-
-    // // Replace with selections from DB
-    // const options = [
-    //     {value: "Workforce Training", label: "Workforce Training"},
-    //     {value: "Information Technology", label: "Information Technology"},
-    // ]
-
-    // const toggleOption = (val) => {
-    //     setSelected(prev => {
-    //         const next = new Set(prev);
-    //         next.has(val) ? next.delete(val) : next.add(val);
-    //         return next;
-    //     })
-    // }
-
 
     return (
         <div>
-            <div>
-                <label>
-                    <span>Analyze Transcript?</span>
-                    <input 
-                        type="checkbox"
-                        checked={conductAnalysis}
-                        onChange={(e) => {
-                            setConductAnalysis(e.target.checked);
-                        }}
-                    />
-                    {/* <input ref={hiddenRef} type="hidden" name="topics" value="[]" /> */}
-                </label>
-            </div>
-            <TopicSelector />
-            <div
-                hidden={!conductAnalysis}
-            >
-                <div
-                    id="topic-selection"
-                >
-                    {/* { selected.size === 0 ? (
-                        <span>No Topics Selected</span>
-                    ) : (
-                        `Selected Topics: ${Array.from(selected).join(', ')}`
-                    )} */}
-                </div>
-                {/* <fieldset role="listbox">
-                    { options.map(opt => {
-                        const checked = selected.has(opt.value);
-                        return (
-                            <span>
-                                <label key={opt.value} role="option">
-                                    <input 
-                                        type="checkbox"
-                                        checked={checked}
-                                        onChange={() => toggleOption(opt.value)}
-                                    />
-                                    <span>{opt.label}</span>
-                                </label>
-                            </span>
-                        )
-                    })}
-                </fieldset> */}
-            </div>
+            <label>
+                <span>Analyze Transcript?</span>
+                <input 
+                    type="checkbox"
+                    checked={conductAnalysis}
+                    onChange={(e) => {
+                        setConductAnalysis(e.target.checked);
+                    }}
+                />
+            </label>
+            <TopicSelector hidden={!conductAnalysis} />
         </div>
     )
 }
