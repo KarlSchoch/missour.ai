57e6ed4201170826357604a0d61bc43951d9e17e
Karl Schoch <karl.schoch@gmail.com>
2025-11-04 05:16:47 -0500

    Creates new model for a topic

--- missourai_django/transcription/models.py
+++ missourai_django/transcription/models.py
@@ -5,3 +5,7 @@ class Transcript(models.Model):
     name = models.CharField(max_length=255)
     transcript_text = models.TextField(default='')
     created_at = models.DateTimeField(auto_now_add=True)
+
+class Topic(models.Model):
+    topic = models.CharField(max_length=100)
+    description = models.CharField(max_length=255, default='', blank=True)
