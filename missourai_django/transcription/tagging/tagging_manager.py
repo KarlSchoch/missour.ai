c333a68019328781e5367f48d6c245a613a884c5
Karl Schoch <karl.schoch@gmail.com>
2025-11-15 06:37:30 -0500

    Implements chunk method within tagging manager to successfully pass tests and adds test to ensure on an object-by-object basiss that all objects returned from the chunk method are added to the database

--- missourai_django/transcription/tagging/tagging_manager.py
+++ missourai_django/transcription/tagging/tagging_manager.py
@@ -1,12 +1,42 @@
 from openai import OpenAI
-from transcription.models import Transcript
+from transcription.models import Transcript, Chunk
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from typing import List
 
 class TaggingManager:
-    def __init__(self, api_key:str):
-        self.client = OpenAI(api_key=api_key)
+    """
+    For an individual transcript, manages chunking and tagging
 
-    def chunk(self, transcript: Transcript):
-        return ""
+    Allows for defining LLM clients and chunking methods
+    """
+    def __init__(
+            self, 
+            api_key:str, 
+            transcript:Transcript = None,
+            chunk_size:int = 500,
+            chunk_overlap:int = 50,
+        ):
+        self.client = OpenAI(api_key=api_key)
+        self.transcript = transcript
+        self.chunker = RecursiveCharacterTextSplitter(
+            chunk_size = chunk_size,
+            chunk_overlap = chunk_overlap
+        )
+
+    def chunk(self) -> List[Chunk]:
+        self.docs = self.chunker.create_documents([self.transcript.transcript_text])
+
+        chunks = []
+
+        for doc in self.docs:
+            chunk_obj = Chunk(
+                transcript = self.transcript,
+                chunk_text = doc.page_content,
+            )
+            chunk_obj.save()
+            chunks.append(chunk_obj)
+
+        return chunks
     
     def tag_chunk(self):
         return ""
