f05079a9aa101ea518fdae7f0d79db5e00df6a16
Karl Schoch <karl.schoch@gmail.com>
2025-11-20 06:06:38 -0500

    Completes core functionality of the TaggingManager.tag_chunks() method.  TO DO: Create mocking for testing and validate that the API call outputs are correctly represented in the mock

--- missourai_django/transcription/tagging/tagging_manager.py
+++ missourai_django/transcription/tagging/tagging_manager.py
@@ -3,6 +3,7 @@ from transcription.models import Transcript, Chunk, Topic, Tag
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from typing import List
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.messages.base import BaseMessage
 from langchain.chat_models import init_chat_model
 from pydantic import BaseModel, Field
 
@@ -30,23 +31,9 @@ class TaggingManager:
             chunk_size = chunk_size,
             chunk_overlap = chunk_overlap
         )
-        self.chunks = []
-        self.topics = topics
-        self.tagging_prompt = ChatPromptTemplate.from_template(
-            """
-            Determine whether the following passage contains a reference to the provided topic.
-            
-            If you are unsure, assume the passage covers the topic as false negatives are more impactful than false positives
-
-            Provide the properties mentioned in the 'Classification' function.
-
-            Topic:
-            {topic}
-            
-            Passage:
-            {passage}
-            """
-        )
+        self.chunks:list[Chunk] = []
+        self.topics:list[Topic] = topics
+        self.tags:list[Tag] = []
 
     def chunk(self) -> List[Chunk]:
         # Use chunker to create langchain "docs"
@@ -63,24 +50,70 @@ class TaggingManager:
 
         return self.chunks
     
-    def tag_chunk(self, chunk:Chunk = None, topic:Topic = None) -> List[Tag]:
+    def tag_chunk(self, chunk:Chunk, topics:List[Topic] = None) -> List[Tag]:
+        # Validate data inputs
+        ## If there is no chunk, provide user an error message
+        ## If there are no topics, fall back onto self.topics
+        if not topics:
+            topics = self.topics
+
         # Set up LLM to enable tagging
         class Classification(BaseModel):
             tag:bool = Field(description="whether the topic is covered in the passage")
             relevant_section:str = Field(
                 description="if you tagged the passage as containing the topic, extract the portion of the passage that led you to this conclusion"
             )
+        # llm is langchain_core.language_models.chat_models.BaseChatModel
         llm = init_chat_model(
             self.tagging_model,
             model_provider=self.model_provider
         )
+        # llm is now langchain_core.runnables.base.Runnable
         llm = llm.with_structured_output(Classification)
 
         # iterate through each tag and topic
-        for topic in self.topics:
+        for topic in topics:
             print(f"* topic: {topic}")
-        
-        return ""
+
+            # tagging_prompt is ChatPromptTemplate class
+            tagging_prompt = ChatPromptTemplate.from_template(
+                """
+                Determine whether the following passage contains a reference to the provided topic.
+                
+                If you are unsure, assume the passage covers the topic as false negatives are more impactful than false positives
+
+                Provide the properties mentioned in the 'Classification' function.
+
+                Topic:
+                {topic}
+                
+                Passage:
+                {passage}
+                """
+            )
+            # prompt is langchain_core.prompt_values.PromptValue
+            prompt = tagging_prompt.invoke({
+                "passage": chunk,
+                "topic": topic
+            })
+
+            # result is langchain_core.messages.base.BaseMessage
+            result = llm.invoke(prompt)
+            assert isinstance(result, BaseMessage)
+            assert hasattr(result, "tag")
+            assert hasattr(result, "relevant_section")
+
+            # Create the Tag object
+            tag_obj = Tag(
+                topic = self.transcript,
+                chunk = chunk,
+                topic_present = result.tag,
+                relevant_section = result.relevant_section
+            )
+            tag_obj.save()
+            self.tags.append(tag_obj)
+
+        return self.tags
     
     def tag_transcript(self):
         """
