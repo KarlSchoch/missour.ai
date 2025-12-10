d5ecf81a93e0eab5c66f54aa9a960e3ff0d375ea
Karl Schoch <karl.schoch@gmail.com>
2025-12-10 05:31:32 -0500

    Writes tests for creating HTML scaffolding to view transcript tags

--- missourai_django/transcription/tests/test_views.py
+++ missourai_django/transcription/tests/test_views.py
@@ -1,64 +1,79 @@
 from django.test import TestCase
 from transcription.models import Transcript, Tag, Topic, Chunk
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class ViewTranscriptTests(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        cls.transcript_one = Transcript.objects.create(
+    def setUp(self):
+        # Authentication
+        self.user = User.objects.create_user(username='test', password='pw')
+        self.client.force_login(self.user)
+        # Base Records
+        self.transcript_one = Transcript.objects.create(
             name = "Dummy Transcript One",
             transcript_text = "Here is some dummy text about IT that should show up"
         )
-        cls.transcript_two = Transcript.objects.create(
+        self.transcript_two = Transcript.objects.create(
             name = "Dummy Transcript Two",
             transcript_text = "Here is some dummy text about IT that should NOT show up"
         )
-        cls.topic_it = Topic.objects.create(
+        self.topic_it = Topic.objects.create(
             topic = "Information Technology",
             description = ""
         )
-        cls.topic_wf = Topic.objects.create(
+        self.topic_wf = Topic.objects.create(
             topic = "Workforce Training",
             description = ""
         )
         # Transcript One Related Elements
-        cls.chunk_transcript_one = Chunk.objects.create(
-            transcript = cls.transcript_one,
-            chunk_text = cls.transcript_one.transcript_text
+        self.chunk_transcript_one = Chunk.objects.create(
+            transcript = self.transcript_one,
+            chunk_text = self.transcript_one.transcript_text
         )
-        cls.tag_topic_it_transcript_one = Tag.objects.create(
-            topic = cls.topic_it,
-            chunk = cls.chunk_transcript_one,
+        self.tag_topic_it_transcript_one = Tag.objects.create(
+            topic = self.topic_it,
+            chunk = self.chunk_transcript_one,
             topic_present = True,
             relevant_section = "text about IT"
         )
-        cls.tag_topic_wf_transcript_one = Tag.objects.create(
-            topic = cls.topic_wf,
-            chunk = cls.chunk_transcript_one,
+        self.tag_topic_wf_transcript_one = Tag.objects.create(
+            topic = self.topic_wf,
+            chunk = self.chunk_transcript_one,
             topic_present = False
         )
         # Transcript Two Related Elements
-        cls.chunk_transcript_two = Chunk.objects.create(
-            transcript = cls.transcript_two,
-            chunk_text = cls.transcript_two.transcript_text
+        self.chunk_transcript_two = Chunk.objects.create(
+            transcript = self.transcript_two,
+            chunk_text = self.transcript_two.transcript_text
         )
-        cls.tag_topic_it_transcript_two = Tag.objects.create(
-            topic = cls.topic_it,
-            chunk = cls.chunk_transcript_two,
+        self.tag_topic_it_transcript_two = Tag.objects.create(
+            topic = self.topic_it,
+            chunk = self.chunk_transcript_two,
             topic_present = True,
             relevant_section = "text about IT"
         )
-        cls.tag_topic_wf_transcript_two = Tag.objects.create(
-            topic = cls.topic_wf,
-            chunk = cls.chunk_transcript_two,
+        self.tag_topic_wf_transcript_two = Tag.objects.create(
+            topic = self.topic_wf,
+            chunk = self.chunk_transcript_two,
             topic_present = False
         )
-
-
         
-    def testViewTranscript(self):
-        # assert that only Dummy Transcript One shows up
-        assert False
-    
-    def testViewChunks(self):
-        # Assert that you only return tags for a single transcript
-        assert False
+    def testViewTranscriptChunks(self):
+        # Call view
+        url = reverse('transcription:view_transcript', args=[self.transcript_one.pk])
+        response = self.client.get(url)
+
+        # Validate backend interaction worked
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'transcription/view_transcript.html')
+
+        # Contains the correct HTML elements
+        ## analyze-audio-page-section-root
+        self.assertContains(response, 'id="view-transcript-chunks-page-section-root"')
+        self.assertContains(response, 'id="view-transcript-chunks-page-section-root"')
+
+        # Initial Payload contains the Correct Data
+        ## Only Dummy Transcript One
+        ## Only tags for a single transcript
