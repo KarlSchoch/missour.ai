06c736b7f1332fca74b30ecf215a196ecbfb7588
Karl Schoch <karl.schoch@gmail.com>
2025-12-13 06:08:12 -0500

    Successfully pulls in topic name as well as chunk text to enable frontend generation

--- missourai_django/transcription/templatetags/transcription_tags.py
+++ missourai_django/transcription/templatetags/transcription_tags.py
@@ -27,6 +27,15 @@ def render_view_transcript_chunks_section(context):
     # Pull the transcript out of the context
     transcript = context.get('transcript')
     # Use that transcript to filer for tags related to that transcript
-    tags = Tag.objects.filter(chunk__transcript__pk = transcript.pk)
+    tags = Tag.objects.filter(chunk__transcript__pk = transcript.pk).select_related('topic', 'chunk').values('topic_present', 'relevant_section', 'topic__topic', 'chunk__chunk_text')
 
     return { 'initial_payload': list(tags.values()) }
+
+[
+    {
+        'topic_present': False,
+        'relevant_section': '',
+        'topic__topic': 'Workforce Training',
+        'chunk__chunk_text': "Transportation, Infrastructure, and Public Safety will come to order, except that we'll go into subcommittee to start because we don't have a quorum yet. So Representative, if you want to hop up here, we'll do your bill. We'll start the testimony on your bill, and then once we get a quorum, we're going to go right into a vote on the roll call for a quorum so we can officially do a committee. So don't mind if we interrupt you in the middle of your testimony. So Representative Busick, go ahead on"
+    }, 
+]
