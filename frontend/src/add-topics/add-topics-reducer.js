a51204262e301c9a119e40a8c4a0e653a013bdb2
Karl Schoch <karl.schoch@gmail.com>
2026-02-05 14:46:28 -0500

    Fixes issue with lack of return from map in update reducer

--- frontend/src/add-topics/add-topics-reducer.js
+++ frontend/src/add-topics/add-topics-reducer.js
@@ -11,14 +11,12 @@ export default function addTopicsReducer(newTopics, action) {
             return newTopics.filter((_, i) => i !== action.index);
         }
         case 'update': {
-            console.log('action', action)
-            console.log('newTopics', newTopics)
-            return newTopics.map((newTopic, i) => {
+            return newTopics.map((newTopic, i) => (
                 i === action.index ? { 
                     ...newTopic,
                     [action.field]: action.value
                 } : newTopic
-            })
+            ))
         }
         default: {
             throw Error('Unknown Action' + action.type)
